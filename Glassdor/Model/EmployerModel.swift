6f3aca96d382aaafd689c38bb769872157cd0cc0
MCS Devices <mcsdevices@MCSs-MacBook-Air-2.local>
2017-12-13 15:48:18 -0500

    - Added file EmployerModel

---
+++ Glassdor/Model/EmployerModel.swift
@@ -0,0 +1,13 @@
+//
+//  EmployerModel.swift
+//  Glassdor
+//
+//  Created by MCS Devices on 12/13/17.
+//  Copyright © 2017 Mobile Consulting Solutions. All rights reserved.
+//
+
+import Foundation
+
+class EmployerModel {
+    
+}
