b9ad1ee993ae7b3c24511d4e890360cd13b272ea
MCS Devices <mcsdevices@MCSs-MacBook-Air.local>
2017-12-14 18:06:53 -0500

    Finish the creation of the dictionary

--- Glassdor/Controllers/FormViewController.swift
+++ Glassdor/Controllers/FormViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FormViewController: UIViewController {
 
+    
     @IBOutlet weak var keyWordsTxt: UITextField!
     @IBOutlet weak var companyTxt: UITextField!
     @IBOutlet weak var locationTxt: UITextField!
@@ -17,50 +18,87 @@ class FormViewController: UIViewController {
     @IBOutlet weak var jobTypePickerView: UIPickerView!
     @IBOutlet weak var categoryPickerView: UIPickerView!
     @IBOutlet weak var raitingPickerView: UIPickerView!
-    @IBOutlet weak var searchBtn: UIButton!
+   
+    var category = ""
+    var jobType = ""
+    var since = ""
+    var rating = ""
+    var keywords = ""
+    var company = ""
+    var location = ""
+    @IBAction func searchBtn(_ sender: Any) {
+        if let key = keyWordsTxt.text{
+            keywords = key
+        }
+        if let comp = companyTxt.text{
+            company = comp
+        }
+        if let loca = locationTxt.text{
+            location = loca
+        }
+        var formDictionary: [String: String] = ["category": category, "jobType": jobType, "since": since, "rating": rating, "keywords": keywords, "company": company, "location": location]
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
 extension FormViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == categoryPickerView {
+            return PickerValues.jobCategories[row]
+        }else if pickerView == sincePickerView{
+            return PickerValues.since[row]
+        }else if pickerView == jobTypePickerView{
+            return PickerValues.jobType[row]
+        }else{
+            return PickerValues.rate[row]
+        }
+    }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == categoryPickerView {
+            category = String(row)
+            print(category)
+        }else if pickerView == sincePickerView{
+            since = String(row)
+            print(since)
+        }else if pickerView == jobTypePickerView{
+            jobType = PickerValues.jobType[row]
+            print(jobType)
+        }else{
+            rating = PickerValues.rate[row]
+            print(rating)
+        }
+    }
 }
 
 extension FormViewController: UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        if pickerView == sincePickerView{
-            return 2
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == categoryPickerView{
+            return PickerValues.jobCategories.count
+        }else if pickerView == sincePickerView{
+            return PickerValues.since.count
+        }else if pickerView == jobTypePickerView{
+            return PickerValues.jobType.count
         }else{
-            return 1
+            return PickerValues.rate.count
         }
         
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
-    }
     
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
-        
-    }
 }
